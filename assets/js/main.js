fdeb747a36ca3bf76f20bf236e2148a400cc220f
MD. MONWAR SIDDIQ <monwarsiddiq@gmail.com>
2022-02-04 21:40:34 +0600

    Changing design V1.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,4 +10,55 @@ document.addEventListener('DOMContentLoaded', () => {
             }    
         });
     }
+
+    document.addEventListener('click', (e) => {
+
+        if(e.target.className === "nav-link home" || e.target.className === "nav-link home2"){
+            document.querySelector('.nav-link.home').classList.add('active');
+            document.querySelector('.nav-link.home2').classList.add('active');
+
+            document.querySelector('.nav-link.features').classList.remove('active');
+            document.querySelector('.nav-link.features2').classList.remove('active');
+            document.querySelector('.nav-link.pricing').classList.remove('active');
+            document.querySelector('.nav-link.pricing2').classList.remove('active');
+            document.querySelector('.nav-link.contact').classList.remove('active');
+            document.querySelector('.nav-link.contact2').classList.remove('active');
+        }
+
+        if(e.target.className === "nav-link features" || e.target.className === "nav-link features2"){
+            document.querySelector('.nav-link.features').classList.add('active');
+            document.querySelector('.nav-link.features2').classList.add('active');
+
+            document.querySelector('.nav-link.home').classList.remove('active');
+            document.querySelector('.nav-link.home2').classList.remove('active');
+            document.querySelector('.nav-link.pricing').classList.remove('active');
+            document.querySelector('.nav-link.pricing2').classList.remove('active');
+            document.querySelector('.nav-link.contact').classList.remove('active');
+            document.querySelector('.nav-link.contact2').classList.remove('active');
+        }
+
+        if(e.target.className === "nav-link pricing" || e.target.className === "nav-link pricing2"){
+            document.querySelector('.nav-link.pricing').classList.add('active');
+            document.querySelector('.nav-link.pricing2').classList.add('active');
+
+            document.querySelector('.nav-link.home').classList.remove('active');
+            document.querySelector('.nav-link.home2').classList.remove('active');
+            document.querySelector('.nav-link.features').classList.remove('active');
+            document.querySelector('.nav-link.features2').classList.remove('active');
+            document.querySelector('.nav-link.contact').classList.remove('active');
+            document.querySelector('.nav-link.contact2').classList.remove('active');
+        }
+        
+        if(e.target.className === "nav-link contact" || e.target.className === "nav-link contact2"){
+            document.querySelector('.nav-link.contact').classList.add('active');
+            document.querySelector('.nav-link.contact2').classList.add('active');
+
+            document.querySelector('.nav-link.home').classList.remove('active');
+            document.querySelector('.nav-link.home2').classList.remove('active');
+            document.querySelector('.nav-link.features').classList.remove('active');
+            document.querySelector('.nav-link.features2').classList.remove('active');
+            document.querySelector('.nav-link.pricing').classList.remove('active');
+            document.querySelector('.nav-link.pricing2').classList.remove('active');
+        }
+    });
 });
